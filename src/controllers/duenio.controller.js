fe4a859576ee6f43f35cd4f8267d5e7f249193d4
juandurso <juandurso1@gmail.com>
2023-07-26 19:07:48 -0300

    agrego CRUD duenios, inicio CRUD pacientes, instalo express validator

--- src/controllers/duenio.controller.js
+++ src/controllers/duenio.controller.js
@@ -9,8 +9,73 @@ const crearDuenio = async(req, res) => {
     });
     await duenio.save()
     res.status(201).json({ message: `duenio registrado ${req.body.nombre}` });
+
+    
 }
 
+const findAllDuenios = async (req, res) => {
+    const nombreRegex = new RegExp(req.query.nombre, "i");
+    const emailRegex = new RegExp(req.query.email, "i");
+    const filters = {
+      nombre: {
+        $regex: nombreRegex,
+      },
+      email: {
+        $regex: emailRegex,
+      },
+    };
+    const duenio = await Duenio.find(filters);
+
+  res.json({ message: "duenio encontrado/s", data: duenio });
+};
+
+const buscarDuenioById = async (req, res) => {
+    const duenio = await Duenio.findById(req.params.id);
+  
+    if (duenio === null) {
+      res.status(404)
+      return res.json({ message: "duenio no encontrado" })
+    }
+  
+    res.json({ message: "FIND DUENIO BY ID", data: duenio });
+  };
+
+  const actualizarDuenioById = async (req, res) => {
+    const duenio = await Duenio.findById(req.params.id);
+  
+    if (duenio === null) {
+      res.status(404)
+      return res.json({ message: "DUENIO NO ENCONTRADO" })
+    }
+  
+    await Duenio.findByIdAndUpdate(req.params.id, {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        telefono: req.body.telefono
+    })
+  
+    res.json({ message: "DUENIO ACTUALIZADO" });
+  };
+
+  const borrarDuenioById = async (req, res) => {
+    const duenio = await Duenio.findById(req.params.id);
+  
+    if (duenio === null) {
+      res.status(404)
+      return res.json({ message: "DUENIO NO ENCONTRADO" })
+    }
+  
+    const filters = { _id: req.params.id }
+    await Duenio.deleteOne(filters)
+  
+    res.json({ message: `DUENIO BORRADO ${duenio.nombre}`});
+  };
+
 module.exports = {
-    crearDuenio
+    crearDuenio,
+    buscarDuenioById,
+    actualizarDuenioById,
+    borrarDuenioById,
+    findAllDuenios
 }
--- src/routes/duenio.route.js
+++ src/routes/duenio.route.js
@@ -1,12 +1,60 @@
 const { Router } = require("express");
-const {crearDuenio} = require("../controllers/duenio.controller")
+const {
+  crearDuenio,
+  actualizarDuenioById,
+  borrarDuenioById,
+  buscarDuenioById,
+  findAllDuenios,
+} = require("../controllers/duenio.controller");
 const { expressValidations } = require("../middlewares/common.validations");
 
+const { body, param } = require("express-validator");
 
 const duenioRouter = Router();
 
 // CREATE http://localhost:8000/duenio/create
 
-duenioRouter.post("/create",crearDuenio )
+duenioRouter.post(
+  "/create",
+  [
+    body("nombre").notEmpty().withMessage("Debe mandar un nombre"),
+    body("apellido").notEmpty().withMessage("Debe mandar un apellido"),
+    body("email").notEmpty().isEmail(),
+    body("telefono").notEmpty(),
+    // body("description", "Debe mandar un description").notEmpty(),
+  ],
+  
+  crearDuenio
+);
 
-module.exports = duenioRouter
+// READALL http://localhost:8000/duenio/find-all
+duenioRouter.get("/find-all", findAllDuenios);
+
+// READBYID http://localhost:8000/duenio/find-by-id/:id
+duenioRouter.get(
+  "/find-by-id/:id",
+  [param("id").isMongoId().withMessage("Debe mandar un ID valido")],
+  buscarDuenioById
+);
+
+// UPDATE http://localhost:8000/duenio/update-by-id/:id
+duenioRouter.put(
+  "/update-by-id/:id",
+  [
+    param("id").isMongoId().withMessage("Debe mandar un ID valido"),
+    body("nombre").isString().withMessage("Debe mandar un nombre"),
+    body("apellido").isString().withMessage("Debe mandar un apellido"),
+    body("email").isEmail().withMessage("debe mandar un email valido"),
+    body("telefono").isNumeric().withMessage("debe mandar un telefono valido"),
+  ],
+  actualizarDuenioById
+);
+
+// DELETE http://localhost:8000/duenio/delete-by-id/:id
+duenioRouter.delete(
+    "/delete-by-id/:id",
+    [param("id").isMongoId().withMessage("Debe mandar un ID valido")],
+    borrarDuenioById
+  );
+
+module.exports = duenioRouter;
